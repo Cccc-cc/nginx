f5e12c664a5cf82df6e9992264415fa2c64a641b
Cccc-cc <85084886+Cccc-cc@users.noreply.github.com>
2021-12-01 17:22:30 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker build -t nginx:v5 .
-docker tag nginx:v5 172.16.207.182:5000/nginx:v6
-docker push 172.16.207.182:5000/nginx:v6
+docker build -t nginx:v1 .
+docker tag nginx:v1 10.0.0.35:5000/nginx:v2
+docker push 10.0.0.35:5000/nginx:v2
